9b16fac315e7061d7cd9577472d26b9bf6da4931
kamyker <kamyker@o2.pl>
2020-01-11 02:52:50 +0100

    float scaling

--- Scripts/NotComponents/MapSettings.cs
+++ Scripts/NotComponents/MapSettings.cs
@@ -5,5 +5,5 @@ public class MapSettings : ScriptableObject
 {
 	public bool DefaultMapEnabled = false;
 	public bool DefaultCloudsEnabled = true;
-	public MinMaxSmoothFloat VisualizeToMusic_GlobalScaling = new MinMaxSmoothFloat(0,1,100, false);
+	public float VisualizeToMusic_GlobalScaling = 1f;
 }
