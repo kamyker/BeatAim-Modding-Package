a30d8c6319feaa5a1bee38a6456b4ab86d430cb3
kamyker <kamyker@o2.pl>
2020-01-13 05:26:41 +0100

    move in world

---
+++ Scripts/Components/MoveObjectWorld.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveObjectWorld : MonoBehaviourEventful<MoveObjectWorld>
+{
+	protected override MoveObjectWorld _this => this;
+
+	[SerializeField] public Vector3 WorldDirection = Vector3.forward;
+	[SerializeField] public float Speed = 1;
+}
