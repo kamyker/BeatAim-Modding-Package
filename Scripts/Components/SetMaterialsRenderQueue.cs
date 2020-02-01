b189df590a06ac20b67c0a32c0f35265c3c6cad1
kamyker <kamyker@o2.pl>
2020-02-01 15:50:03 +0100

    redner queue component

---
+++ Scripts/Components/SetMaterialsRenderQueue.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetMaterialsRenderQueue : MonoBehaviour
+{
+	List<KeyValuePair<Material, int>> materials = new List<KeyValuePair<Material, int>>();
+
+
+	private void Awake()
+	{
+		foreach ( var m in materials )
+			m.Key.renderQueue = m.Value;
+	}
+}
