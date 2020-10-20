8c212a60589b1f4dcde3ae931d8973252c78413c
kamyker <kamyker@o2.pl>
2020-10-20 20:48:50 +0200

    PlayerCart prorioty

--- Scripts/Components/PlayerCart.cs
+++ Scripts/Components/PlayerCart.cs
@@ -5,5 +5,13 @@ using UnityEngine;
 
 public class PlayerCart : MonoBehaviourEventful<PlayerCart>
 {
-    protected override PlayerCart _this => this;
+	protected override PlayerCart _this => this;
+	public int Priority = 0;
+	public Animator Animator;
+
+	private void Reset()
+	{
+		if ( Animator == null )
+			Animator = GetComponent<Animator>();
+	}
 }
