68b72fa6afff115da06fb1fbaa34a75064efcaf0
kamyker <kamyker@o2.pl>
2022-01-24 15:20:53 +0100

    video more changes

--- Scripts/Components/ObjectWithVideo.cs
+++ Scripts/Components/ObjectWithVideo.cs
@@ -3,4 +3,5 @@
 public class ObjectWithVideo : MonoBehaviourEventful<ObjectWithVideo>
 {
 	[SerializeField] public bool DisableGameObjectWhenVideoNotAvailable = true;
+	[SerializeField] public MeshRenderer MeshToReceiveVideoMaterial;
 }
