4b0bbdf67632658aac67d74e43c6b645d24d9a7a
kamyker <kamyker@o2.pl>
2022-01-24 15:27:33 +0100

    more settings billboards

--- Scripts/NotComponents/MapSettings.cs
+++ Scripts/NotComponents/MapSettings.cs
@@ -5,5 +5,6 @@ public class MapSettings : ScriptableObject
 {
 	public bool DefaultMapEnabled = false;
 	public bool DefaultCloudsEnabled = true;
+	public bool DefaultVideoBillboardsEnabled = true;
 	public float VisualizeToMusic_GlobalScaling = 1f;
 }
